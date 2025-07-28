dca23f4dcee94cbb80b3354f4fa201853d162546
Rudra1385 <crudra1385@gmail.com>
2025-07-28 17:58:00 -0400

    lab4/part2/main java file
    Added the toggling button for darkening and lightening the image.

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -40,3 +40,15 @@ for (let i = 1; i <= imageNames.length; i++) {
 
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function () {
+  const btnClass = btn.getAttribute('class'); 
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+  }
+});
