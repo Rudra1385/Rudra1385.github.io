2618b5d9dbf2ee6aba556f6f5de5397b9966198c
Rudra1385 <crudra1385@gmail.com>
2025-07-29 20:20:43 -0400

    lab4/part3/main.js file
    Added a function which updates the data of ball.

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -36,11 +36,33 @@ class Ball {
     this.size = size;
   }
   // function for drawing ball
- draw() {
+  draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+  // function for updating ball's data
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
 }
+
 
